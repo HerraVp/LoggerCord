c75de4ecc2e9bb762af4a2e5731025908f2d4b53
HerraVp <Vp2b2t@gmail.com>
2022-03-03 17:43:42 +0200

    wtf im dumb

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
 # Tutorial how to get your discord token
 # https://www.youtube.com/watch?v=LnBnm_tZlyU
-token = "mfa.SfG2kp_uOJ5YATggFMpbeS_dEEXgUVmy7YfqHsI58aMK2Xw1K_04X2WKesgIcjpb7odu0Mm0uzlK1LtFFE2d"
+token = ""
